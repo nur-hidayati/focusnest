c69ae4f0979d934b75ca15994f43f5bd0e1a0b08
nur-hidayati <hidayati.coder@gmail.com>
2024-05-11 03:22:02 +0800

    initializeApp Firebase

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:focusnest/app.dart';
+import 'package:focusnest/firebase_options_prod.dart' as prod;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: prod.DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App(flavor: 'PRODUCTION'));
 }
