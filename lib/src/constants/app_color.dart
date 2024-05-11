9fa019096b30395afd4e88af965b69b0b303bf22
nur-hidayati <hidayati.coder@gmail.com>
2024-05-11 17:02:12 +0800

    add AppColor const

---
+++ lib/src/constants/app_color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const primaryColor = Color.fromARGB(255, 126, 180, 222);
+  static const secondaryColor = Color(0xFFD6E6F2);
+  static const inputColor = Color.fromARGB(255, 245, 245, 245);
+}
