75264d679b363b0695e74fa3e2cad79211f13e26
nur-hidayati <hidayati.coder@gmail.com>
2024-05-12 16:42:36 +0800

    add RoutesName const

---
+++ lib/src/constants/routes_name.dart
@@ -0,0 +1,6 @@
+class RoutesName {
+  static const String auth = 'auth';
+  static const String activityTimer = 'activityTimer';
+  static const String activityCalendar = 'activityCalendar';
+  static const String settings = 'settings';
+}
