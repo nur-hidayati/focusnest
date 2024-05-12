3f6c838de05217c88a45d6c9eac04f0b917985e9
nur-hidayati <hidayati.coder@gmail.com>
2024-05-13 00:18:34 +0800

    set isFullWidth as false in CustomButton

--- lib/src/common_widgets/custom_button.dart
+++ lib/src/common_widgets/custom_button.dart
@@ -5,7 +5,7 @@ class CustomButton extends StatelessWidget {
   const CustomButton({
     super.key,
     required this.title,
-    this.isFullWidth = true,
+    this.isFullWidth = false,
     this.onPressed,
   });
 
