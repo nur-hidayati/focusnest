6f41dcfde30461a55657b0526e146258e2285cb7
nur-hidayati <hidayati.coder@gmail.com>
2024-05-13 03:27:09 +0800

    add timerStart to routes

--- lib/src/constants/routes_name.dart
+++ lib/src/constants/routes_name.dart
@@ -1,6 +1,9 @@
 class RoutesName {
   static const String auth = 'auth';
-  static const String activityTimer = 'activityTimer';
   static const String activityCalendar = 'activityCalendar';
   static const String settings = 'settings';
+
+  // Timer
+  static const String activityTimer = 'activityTimer';
+  static const String timerStart = 'timerStart';
 }
