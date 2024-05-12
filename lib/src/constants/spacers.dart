bf4543b55e5340c31c0d446a8d3f9388d6696f02
nur-hidayati <hidayati.coder@gmail.com>
2024-05-13 00:18:55 +0800

    update Spacers

--- lib/src/constants/spacers.dart
+++ lib/src/constants/spacers.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Spacers {
+  static const Widget extraSmallVertical = SizedBox(height: 5);
   static const Widget smallVertical = SizedBox(height: 10);
   static const Widget mediumVertical = SizedBox(height: 20);
   static const Widget largeVertical = SizedBox(height: 30);
 
+  static const Widget extraSmallHorizontal = SizedBox(width: 5);
   static const Widget smallHorizontal = SizedBox(width: 10);
   static const Widget mediumHorizontal = SizedBox(width: 20);
   static const Widget largeHorizontal = SizedBox(width: 30);
