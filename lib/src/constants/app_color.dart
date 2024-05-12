c67b827d24adbcee4f354d05417c0d8321cddeb9
nur-hidayati <hidayati.coder@gmail.com>
2024-05-12 16:43:24 +0800

    add greyColor to const

--- lib/src/constants/app_color.dart
+++ lib/src/constants/app_color.dart
@@ -4,4 +4,5 @@ class AppColor {
   static const primaryColor = Color.fromARGB(255, 126, 180, 222);
   static const secondaryColor = Color(0xFFD6E6F2);
   static const inputColor = Color.fromARGB(255, 245, 245, 245);
+  static const greyColor = Color(0xFF666666);
 }
