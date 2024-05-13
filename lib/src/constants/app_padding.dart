cabf4c43f6d11802216e05cfabe078883c248e5d
nur-hidayati <hidayati.coder@gmail.com>
2024-05-13 23:33:23 +0800

    add AppPadding noBottomPadding

--- lib/src/constants/app_padding.dart
+++ lib/src/constants/app_padding.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppPadding {
   static const EdgeInsets screenPadding = EdgeInsets.all(20.0);
+  static const EdgeInsets noBottomPadding = EdgeInsets.fromLTRB(20, 20, 20, 0);
 }
