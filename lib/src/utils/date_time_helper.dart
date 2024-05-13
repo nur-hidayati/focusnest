f9331dfaf7d494103c00feea331b615e2e072092
nur-hidayati <hidayati.coder@gmail.com>
2024-05-13 19:51:54 +0800

    add formatDuration to datetime helper

---
+++ lib/src/utils/date_time_helper.dart
@@ -0,0 +1,6 @@
+String formatDuration(Duration duration) {
+  String twoDigits(int n) => n.toString().padLeft(2, '0');
+  String hours = twoDigits(duration.inHours);
+  String minutes = twoDigits(duration.inMinutes.remainder(60));
+  return duration.inHours > 0 ? '$hours:$minutes:00' : '00:$minutes';
+}
