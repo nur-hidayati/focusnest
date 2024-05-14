08c1629074826ff0bddf9fe6a295085f9b6e8eaa
nur-hidayati <hidayati.coder@gmail.com>
2024-05-14 18:46:57 +0800

    mockup ActivityCalendarScreen

--- lib/src/features/activity_calendar/presentation/activity_calendar_screen.dart
+++ lib/src/features/activity_calendar/presentation/activity_calendar_screen.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:focusnest/src/features/activity_timer/data/activity_timer_database.dart';
+
+// class ActivityCalendarScreen extends StatelessWidget {
+//   const ActivityCalendarScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Calendar'),
+//       ),
+//     );
+//   }
+// }
 
 class ActivityCalendarScreen extends StatelessWidget {
   const ActivityCalendarScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dao = ActivityTimerDatabase().activityTimersDao;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Calendar'),
+      ),
+      body: StreamBuilder<List<ActivityTimer>>(
+        stream: dao.watchAllActivityTimers(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No activities found.'));
+          }
+          final activities = snapshot.data!;
+          return ListView.builder(
+            itemCount: activities.length,
+            itemBuilder: (context, index) {
+              final activity = activities[index];
+              return ListTile(
+                title: Text(activity.activityLabel),
+                subtitle:
+                    Text('Duration: ${activity.durationInSeconds} seconds\n'
+                        'Start: ${activity.startDateTime}\n'
+                        'End: ${activity.endDateTime}'),
+              );
+            },
+          );
+        },
       ),
     );
   }
