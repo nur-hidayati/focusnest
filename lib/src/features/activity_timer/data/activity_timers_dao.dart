e423682315f655a1df9430f65c1fdf1565efa0c8
nur-hidayati <hidayati.coder@gmail.com>
2024-05-14 18:47:05 +0800

    add ActivityTimersDao

---
+++ lib/src/features/activity_timer/data/activity_timers_dao.dart
@@ -0,0 +1,43 @@
+import 'package:drift/drift.dart';
+
+import 'activity_timer_database.dart';
+
+part 'activity_timers_dao.g.dart';
+
+@DriftAccessor(tables: [ActivityTimers])
+class ActivityTimersDao extends DatabaseAccessor<ActivityTimerDatabase>
+    with _$ActivityTimersDaoMixin {
+  // ignore: use_super_parameters
+  ActivityTimersDao(ActivityTimerDatabase db) : super(db);
+
+  // Stream to watch all activity timers
+  Stream<List<ActivityTimer>> watchAllActivityTimers() {
+    return select(activityTimers).watch();
+  }
+
+  // Stream to watch a specific activity timer by ID
+  Stream<ActivityTimer?> watchActivityTimerById(String id) {
+    return (select(activityTimers)..where((t) => t.id.equals(id)))
+        .watchSingleOrNull();
+  }
+
+  // Insert a new activity timer
+  Future<int> insertActivityTimer(ActivityTimersCompanion entry) {
+    return into(activityTimers).insert(entry);
+  }
+
+  // Update an existing activity timer
+  Future<bool> updateActivityTimer(ActivityTimersCompanion entry) {
+    return update(activityTimers).replace(entry);
+  }
+
+  // Delete an activity timer by ID
+  Future<int> deleteActivityTimerById(String id) {
+    return (delete(activityTimers)..where((t) => t.id.equals(id))).go();
+  }
+
+  // Fetch all activity timers
+  Future<List<ActivityTimer>> getAllActivityTimers() {
+    return select(activityTimers).get();
+  }
+}
---
+++ lib/src/features/activity_timer/data/activity_timers_dao.g.dart
@@ -0,0 +1,8 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'activity_timers_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$ActivityTimersDaoMixin on DatabaseAccessor<ActivityTimerDatabase> {
+  $ActivityTimersTable get activityTimers => attachedDatabase.activityTimers;
+}
