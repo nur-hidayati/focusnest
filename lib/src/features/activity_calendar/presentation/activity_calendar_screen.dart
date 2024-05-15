053a1c11bfb22d0e92394d27e5f91c37abdcf991
nur-hidayati <hidayati.coder@gmail.com>
2024-05-16 05:05:26 +0800

    display timer data in calendar

--- lib/src/features/activity_calendar/presentation/activity_calendar_screen.dart
+++ lib/src/features/activity_calendar/presentation/activity_calendar_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:focusnest/src/common_widgets/custom_text.dart';
 import 'package:focusnest/src/common_widgets/user_not_found.dart';
+import 'package:focusnest/src/constants/app_color.dart';
+import 'package:focusnest/src/constants/spacers.dart';
 import 'package:focusnest/src/features/activity_calendar/presentation/custom_calendar.dart';
 import 'package:focusnest/src/features/activity_timer/data/activity_timer_database.dart';
 import 'package:focusnest/src/features/activity_timer/data/activity_timer_providers.dart';
 import 'package:focusnest/src/features/authentication/data/auth_repository.dart';
+import 'package:focusnest/src/utils/date_time_helper.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class ActivityCalendarScreen extends ConsumerStatefulWidget {
@@ -57,16 +61,49 @@ class _ActivityCalendarScreenState
                       final activities = snapshot.data!;
                       return ListView.builder(
                         shrinkWrap: true,
+                        padding: EdgeInsets.zero,
                         itemCount: activities.length,
                         itemBuilder: (context, index) {
                           final activity = activities[index];
-                          return ListTile(
-                            title: Text(activity.activityLabel),
-                            subtitle: Text(
-                                'Duration: ${activity.actualDurationInSeconds} seconds\n'
-                                'Start: ${activity.startDateTime}\n'
-                                'End: ${activity.endDateTime}\n'
-                                'userId: ${activity.userId}'),
+                          return Column(
+                            children: [
+                              const Divider(),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 20),
+                                child: ListTile(
+                                    contentPadding: EdgeInsets.zero,
+                                    dense: true,
+                                    title: CustomText(
+                                      title: formatSecondsToReadable(Duration(
+                                          seconds: activity
+                                              .actualDurationInSeconds)),
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    subtitle: CustomText(
+                                      title: activity.activityLabel,
+                                    ),
+                                    trailing: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        CustomText(
+                                          title: formatTime(
+                                              activity.startDateTime),
+                                          fontSize: 14,
+                                          color: AppColor.greyColor,
+                                        ),
+                                        Spacers.extraSmallVertical,
+                                        CustomText(
+                                          title:
+                                              formatTime(activity.endDateTime),
+                                          fontSize: 14,
+                                          color: AppColor.greyColor,
+                                        ),
+                                      ],
+                                    )),
+                              ),
+                            ],
                           );
                         },
                       );
