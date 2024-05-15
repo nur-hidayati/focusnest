fccae8d30c1b8c35578fe9a28a12d51189861477
nur-hidayati <hidayati.coder@gmail.com>
2024-05-16 05:24:11 +0800

    add activityCalendarDaoProvider

---
+++ lib/src/features/activity_calendar/data/activity_calendar_providers.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:focusnest/src/features/activity_timer/data/activity_timer_providers.dart';
+
+import 'activity_calendar_dao.dart';
+
+final activityCalendarDaoProvider = Provider<ActivityCalendarDao>((ref) {
+  final db = ref.watch(activityTimerDatabaseProvider);
+  return ActivityCalendarDao(db);
+});
