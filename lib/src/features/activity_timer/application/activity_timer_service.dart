7ba8cd2c383db26809e1ff685b8b949264c164a5
nur-hidayati <hidayati.coder@gmail.com>
2024-05-16 02:26:53 +0800

    update timer service

--- lib/src/features/activity_timer/application/activity_timer_service.dart
+++ lib/src/features/activity_timer/application/activity_timer_service.dart
@@ -3,37 +3,48 @@ import 'package:focusnest/src/features/activity_timer/data/activity_timer_databa
 import 'package:focusnest/src/features/activity_timer/data/activity_timers_dao.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-// Define keys for SharedPreferences
-const String activityLabelKey = 'activityLabel';
-const String timerDurationKey = 'timerDurationInSeconds';
+const String activityLabelKeySuffix = 'activityLabel';
+const String timerDurationKeySuffix = 'timerDurationInSeconds';
+const String deletedItemIdsSuffix = 'deletedItemIds';
 const String initialActivityLabelValue = 'Work';
 const Duration initialTimerDurationValue = Duration(minutes: 15);
 
+String getActivityLabelKey(String userId) => '$userId-$activityLabelKeySuffix';
+String getTimerDurationKey(String userId) => '$userId-$timerDurationKeySuffix';
+String getDeletedItemIdsKey(String userId) => '$userId-$deletedItemIdsSuffix';
+
 class ActivityLabelNotifier extends StateNotifier<String> {
-  ActivityLabelNotifier() : super(initialActivityLabelValue) {
+  final String userId;
+
+  ActivityLabelNotifier(this.userId) : super(initialActivityLabelValue) {
     _loadActivityLabel();
   }
 
   void _loadActivityLabel() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    state = prefs.getString(activityLabelKey) ?? initialActivityLabelValue;
+    String key = getActivityLabelKey(userId);
+    state = prefs.getString(key) ?? initialActivityLabelValue;
   }
 
   void updateActivityLabel(String newLabel) async {
     state = newLabel;
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString(activityLabelKey, newLabel);
+    String key = getActivityLabelKey(userId);
+    await prefs.setString(key, newLabel);
   }
 }
 
 class TimerDurationNotifier extends StateNotifier<Duration> {
-  TimerDurationNotifier() : super(initialTimerDurationValue) {
+  final String userId;
+
+  TimerDurationNotifier(this.userId) : super(initialTimerDurationValue) {
     _loadTimerDuration();
   }
 
   void _loadTimerDuration() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    final durationInSeconds = prefs.getInt(timerDurationKey);
+    String key = getTimerDurationKey(userId);
+    final durationInSeconds = prefs.getInt(key);
     if (durationInSeconds != null) {
       state = Duration(seconds: durationInSeconds);
     } else {
@@ -44,23 +55,26 @@ class TimerDurationNotifier extends StateNotifier<Duration> {
   void updateTimerDuration(Duration newDuration) async {
     state = newDuration;
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setInt(timerDurationKey, newDuration.inSeconds);
+    String key = getTimerDurationKey(userId);
+    await prefs.setInt(key, newDuration.inSeconds);
   }
 }
 
 class RecentActivitiesNotifier extends StateNotifier<List<ActivityTimer>> {
-  RecentActivitiesNotifier(this._dao) : super([]) {
+  final ActivityTimersDao _dao;
+  final String userId;
+  List<String> _deletedItemIds = [];
+
+  RecentActivitiesNotifier(this._dao, this.userId) : super([]) {
     _loadRecentActivities();
   }
 
-  final ActivityTimersDao _dao;
-  List<String> _deletedItemIds = [];
-
   Future<void> _loadRecentActivities() async {
     final prefs = await SharedPreferences.getInstance();
-    _deletedItemIds = prefs.getStringList('deletedItemIds') ?? [];
+    String key = getDeletedItemIdsKey(userId);
+    _deletedItemIds = prefs.getStringList(key) ?? [];
 
-    _dao.watchRecentActivities().listen((recentActivities) {
+    _dao.watchRecentActivities(userId).listen((recentActivities) {
       final filteredActivities = recentActivities
           .where((activity) => !_deletedItemIds.contains(activity.id))
           .toList();
@@ -71,7 +85,8 @@ class RecentActivitiesNotifier extends StateNotifier<List<ActivityTimer>> {
   Future<void> removeActivity(ActivityTimer activity) async {
     _deletedItemIds.add(activity.id);
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setStringList('deletedItemIds', _deletedItemIds);
+    String key = getDeletedItemIdsKey(userId);
+    await prefs.setStringList(key, _deletedItemIds);
 
     state = List.from(state..remove(activity));
   }
