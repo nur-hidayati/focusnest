16fb046572d926f87e91fa493a1e5b33ddf55f44
nur-hidayati <hidayati.coder@gmail.com>
2024-05-16 05:44:08 +0800

    update ActivityCalendarScreen

--- lib/src/features/activity_calendar/presentation/activity_calendar_screen.dart
+++ lib/src/features/activity_calendar/presentation/activity_calendar_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:focusnest/src/common_widgets/custom_text.dart';
 import 'package:focusnest/src/common_widgets/user_not_found.dart';
 import 'package:focusnest/src/constants/app_color.dart';
@@ -8,6 +9,7 @@ import 'package:focusnest/src/features/activity_calendar/data/activity_calendar_
 import 'package:focusnest/src/features/activity_calendar/presentation/custom_calendar.dart';
 import 'package:focusnest/src/features/activity_timer/data/activity_timer_database.dart';
 import 'package:focusnest/src/features/authentication/data/auth_repository.dart';
+import 'package:focusnest/src/utils/alert_dialogs.dart';
 import 'package:focusnest/src/utils/date_time_helper.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -83,6 +85,8 @@ class _ActivityCalendarScreenState
                         padding: EdgeInsets.zero,
                         itemCount: activities.length,
                         itemBuilder: (context, index) {
+                          final dao =
+                              ActivityTimerDatabase().activityCalendarDao;
                           final activity = activities[index];
                           return Column(
                             children: [
@@ -90,34 +94,62 @@ class _ActivityCalendarScreenState
                               Padding(
                                 padding:
                                     const EdgeInsets.symmetric(horizontal: 20),
-                                child: ListTile(
-                                  contentPadding: EdgeInsets.zero,
-                                  dense: true,
-                                  title: CustomText(
-                                    title: formatSecondsToReadable(Duration(
-                                        seconds:
-                                            activity.actualDurationInSeconds)),
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  subtitle: CustomText(
-                                    title: activity.activityLabel,
-                                  ),
-                                  trailing: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
+                                child: Slidable(
+                                  key: ValueKey(activity.id),
+                                  closeOnScroll: true,
+                                  endActionPane: ActionPane(
+                                    motion: const ScrollMotion(),
                                     children: [
-                                      CustomText(
-                                        title:
-                                            formatTime(activity.startDateTime),
-                                        fontSize: 14,
-                                        color: AppColor.greyColor,
-                                      ),
-                                      Spacers.extraSmallVertical,
-                                      CustomText(
-                                        title: formatTime(activity.endDateTime),
-                                        fontSize: 14,
-                                        color: AppColor.greyColor,
+                                      SlidableAction(
+                                        onPressed: (context) async {
+                                          bool? isDeleteConfirm =
+                                              await showAlertDialog(
+                                                  context: context,
+                                                  title: 'Delete Record',
+                                                  content:
+                                                      'Are you sure want to delete this record permanently? You cannot undo this action.');
+                                          if (isDeleteConfirm == true) {
+                                            await dao.deleteActivityTimerById(
+                                                activity.id, userId);
+                                          }
+                                        },
+                                        icon: Icons.delete,
+                                        backgroundColor: Colors.red,
+                                        label: 'Delete',
                                       ),
                                     ],
+                                  ),
+                                  child: ListTile(
+                                    contentPadding: EdgeInsets.zero,
+                                    dense: true,
+                                    title: CustomText(
+                                      title: formatSecondsToReadable(Duration(
+                                          seconds: activity
+                                              .actualDurationInSeconds)),
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    subtitle: CustomText(
+                                      title: activity.activityLabel,
+                                    ),
+                                    trailing: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        CustomText(
+                                          title: formatTime(
+                                              activity.startDateTime),
+                                          fontSize: 14,
+                                          color: AppColor.greyColor,
+                                        ),
+                                        Spacers.extraSmallVertical,
+                                        CustomText(
+                                          title:
+                                              formatTime(activity.endDateTime),
+                                          fontSize: 14,
+                                          color: AppColor.greyColor,
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ),
