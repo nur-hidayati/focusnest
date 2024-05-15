bb678ae4d87c0493b62922629136746da29e5ad2
nur-hidayati <hidayati.coder@gmail.com>
2024-05-16 04:21:35 +0800

    add CustomCalendar to ActivityCalendarScreen

--- lib/src/features/activity_calendar/presentation/activity_calendar_screen.dart
+++ lib/src/features/activity_calendar/presentation/activity_calendar_screen.dart
@@ -1,31 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:focusnest/src/common_widgets/user_not_found.dart';
+import 'package:focusnest/src/features/activity_calendar/presentation/custom_calendar.dart';
 import 'package:focusnest/src/features/activity_timer/data/activity_timer_database.dart';
 import 'package:focusnest/src/features/activity_timer/data/activity_timer_providers.dart';
 import 'package:focusnest/src/features/authentication/data/auth_repository.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-// class ActivityCalendarScreen extends StatelessWidget {
-//   const ActivityCalendarScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Calendar'),
-//       ),
-//     );
-//   }
-// }
-
-class ActivityCalendarScreen extends ConsumerWidget {
+class ActivityCalendarScreen extends ConsumerStatefulWidget {
   const ActivityCalendarScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ActivityCalendarScreen> createState() =>
+      _ActivityCalendarScreenState();
+}
+
+class _ActivityCalendarScreenState
+    extends ConsumerState<ActivityCalendarScreen> {
+  DateTime _selectedDate = DateTime.now();
+
+  void _handleOnDateSelectionChanged(DateTime newDate) {
+    setState(() {
+      _selectedDate = newDate;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final authRepository = ref.watch(authRepositoryProvider);
     final userId = authRepository.currentUser?.uid;
-
     final dao = ref.watch(activityTimersDaoProvider);
 
     return Scaffold(
@@ -33,31 +36,44 @@ class ActivityCalendarScreen extends ConsumerWidget {
         title: const Text('Calendar'),
       ),
       body: userId != null
-          ? StreamBuilder<List<ActivityTimer>>(
-              stream: dao.watchAllActivityTimers(userId),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(child: Text('No activities found.'));
-                }
-                final activities = snapshot.data!;
-                return ListView.builder(
-                  itemCount: activities.length,
-                  itemBuilder: (context, index) {
-                    final activity = activities[index];
-                    return ListTile(
-                      title: Text(activity.activityLabel),
-                      subtitle: Text(
-                          'Duration: ${activity.actualDurationInSeconds} seconds\n'
-                          'Start: ${activity.startDateTime}\n'
-                          'End: ${activity.endDateTime}\n'
-                          'userId: ${activity.userId}'),
-                    );
-                  },
-                );
-              },
+          ? Column(
+              children: [
+                CustomCalendar(
+                  selectedDate: _selectedDate,
+                  onDateSelectedChanged: _handleOnDateSelectionChanged,
+                  calendarFormat: CalendarFormat.week,
+                ),
+                Expanded(
+                  child: StreamBuilder<List<ActivityTimer>>(
+                    stream: dao.watchAllActivityTimers(userId),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+                      if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                        return const Center(
+                            child: Text('No activities found.'));
+                      }
+                      final activities = snapshot.data!;
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: activities.length,
+                        itemBuilder: (context, index) {
+                          final activity = activities[index];
+                          return ListTile(
+                            title: Text(activity.activityLabel),
+                            subtitle: Text(
+                                'Duration: ${activity.actualDurationInSeconds} seconds\n'
+                                'Start: ${activity.startDateTime}\n'
+                                'End: ${activity.endDateTime}\n'
+                                'userId: ${activity.userId}'),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
             )
           : const UserNotFound(),
     );
