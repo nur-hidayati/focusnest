72d450f53aa64d34966e0d21cab98d9dc515a01f
nur-hidayati <hidayati.coder@gmail.com>
2024-05-16 18:31:08 +0800

    update ActivityLabelForm

--- lib/src/features/activity_timer/presentation/activity_label_form.dart
+++ lib/src/features/activity_timer/presentation/activity_label_form.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:focusnest/src/common_widgets/bottom_sheet_header.dart';
+import 'package:focusnest/src/common_widgets/bottom_sheet_contents.dart';
 import 'package:focusnest/src/common_widgets/custom_text_form_field.dart';
-import 'package:focusnest/src/constants/app_padding.dart';
-import 'package:focusnest/src/constants/spacers.dart';
 import 'package:focusnest/src/features/activity_timer/data/activity_timer_providers.dart';
 import 'package:focusnest/src/features/authentication/data/auth_repository.dart';
 import 'package:focusnest/src/utils/alert_dialogs.dart';
@@ -71,31 +69,13 @@ class _ActivityLabelFormState extends ConsumerState<ActivityLabelForm> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: EdgeInsets.only(
-        bottom: MediaQuery.of(context).viewInsets.bottom,
-      ),
-      child: Column(
-        children: [
-          BottomSheetHeader(
-            title: 'Activity Label',
-            onDone: () => _handleOnDoneActivityLabelUpdate(),
-            onCancel: () => context.pop(),
-          ),
-          Padding(
-            padding: AppPadding.screenPadding,
-            child: Column(
-              children: [
-                CustomTextFormField(
-                  controller: activityLabelController,
-                  hintText: 'Activity Label',
-                  maxLength: 40,
-                ),
-              ],
-            ),
-          ),
-          Spacers.extraLargeVertical,
-        ],
+    return BottomSheetContents(
+      headerTitle: 'Edit Record',
+      onDoneActivityLabelUpdate: () => _handleOnDoneActivityLabelUpdate(),
+      child: CustomTextFormField(
+        controller: activityLabelController,
+        hintText: 'Activity Label',
+        maxLength: 40,
       ),
     );
   }
