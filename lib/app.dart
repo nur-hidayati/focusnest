79ed349b84aa39ea8d6df8c79c2aa9bc17d5da5e
nur-hidayati <hidayati.coder@gmail.com>
2024-05-18 04:16:39 +0800

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,12 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:focusnest/src/constants/strings.dart';
 import 'package:focusnest/src/routing/app_router.dart';
+import 'package:focusnest/src/services/notfication_controller.dart';
 import 'package:focusnest/src/utils/theme.dart';
 
-class App extends ConsumerWidget {
+class App extends ConsumerStatefulWidget {
   final String flavor;
 
   const App({
@@ -13,7 +15,26 @@ class App extends ConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<App> createState() => _AppState();
+}
+
+class _AppState extends ConsumerState<App> {
+  @override
+  void initState() {
+    super.initState();
+    AwesomeNotifications().setListeners(
+      onActionReceivedMethod: NotificationController.onActionReceivedMethod,
+      onNotificationCreatedMethod:
+          NotificationController.onNotificationCreatedMethod,
+      onNotificationDisplayedMethod:
+          NotificationController.onNotificationDisplayedMethod,
+      onDismissActionReceivedMethod:
+          NotificationController.onDismissActionReceivedMethod,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final goRouter = ref.watch(goRouterProvider);
     return GestureDetector(
       behavior: HitTestBehavior.opaque,
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -5,7 +5,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:focusnest/app.dart';
 import 'package:focusnest/firebase_options_dev.dart' as dev;
-import 'package:focusnest/src/constants/app_color.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,25 +17,29 @@ void main() async {
   );
 
   AwesomeNotifications().initialize(
-    null,
-    // 'resource://drawable/res_app_icon',
-    [
-      NotificationChannel(
-        channelGroupKey: 'basic_channel_group',
-        channelKey: 'basic_channel',
-        channelName: 'Basic notifications',
-        channelDescription: 'Basic Notifications channel',
-        defaultColor: AppColor.primaryColor,
-        ledColor: Colors.white,
-      )
-    ],
-    channelGroups: [
-      NotificationChannelGroup(
+      null,
+      // 'resource://drawable/res_app_icon',
+      [
+        NotificationChannel(
           channelGroupKey: 'basic_channel_group',
-          channelGroupName: 'Basic group')
-    ],
-    debug: true,
-  );
+          channelKey: 'basic_channel',
+          channelName: 'Basic notifications',
+          channelDescription: 'Basic Notifications channel',
+          defaultColor: Colors.green,
+          importance: NotificationImportance.High,
+          ledColor: Colors.white,
+        )
+      ], channelGroups: [
+    NotificationChannelGroup(
+        channelGroupKey: 'basic_channel_group', channelGroupName: 'Basic group')
+  ]);
+
+  bool isAllowedSendNotification =
+      await AwesomeNotifications().isNotificationAllowed();
+
+  if (!isAllowedSendNotification) {
+    AwesomeNotifications().requestPermissionToSendNotifications();
+  }
 
   runApp(
     const ProviderScope(
