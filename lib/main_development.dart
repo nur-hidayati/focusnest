13f0d2d7494bd9442793426985978b0237da44c8
nur-hidayati <hidayati.coder@gmail.com>
2024-05-18 00:47:04 +0800

    initialize notification in main

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,9 +1,11 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:focusnest/app.dart';
 import 'package:focusnest/firebase_options_dev.dart' as dev;
+import 'package:focusnest/src/constants/app_color.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,6 +15,27 @@ void main() async {
 
   await Firebase.initializeApp(
     options: dev.DefaultFirebaseOptions.currentPlatform,
+  );
+
+  AwesomeNotifications().initialize(
+    null,
+    // 'resource://drawable/res_app_icon',
+    [
+      NotificationChannel(
+        channelGroupKey: 'basic_channel_group',
+        channelKey: 'basic_channel',
+        channelName: 'Basic notifications',
+        channelDescription: 'Basic Notifications channel',
+        defaultColor: AppColor.primaryColor,
+        ledColor: Colors.white,
+      )
+    ],
+    channelGroups: [
+      NotificationChannelGroup(
+          channelGroupKey: 'basic_channel_group',
+          channelGroupName: 'Basic group')
+    ],
+    debug: true,
   );
 
   runApp(
