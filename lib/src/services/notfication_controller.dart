712f4add289dd6a1fd836ff1a3f6eca501e4b0d8
nur-hidayati <hidayati.coder@gmail.com>
2024-05-18 16:30:48 +0800

    add NotificationController

--- lib/src/services/notfication_controller.dart
+++ lib/src/services/notfication_controller.dart
@@ -1,8 +1,42 @@
 import 'dart:async';
 
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:focusnest/src/common_widgets/custom_text.dart';
+import 'package:focusnest/src/constants/app_color.dart';
+import 'package:go_router/go_router.dart';
 
 class NotificationController {
+  static ReceivedAction? initialAction;
+
+  static Future<void> initializeLocalNotifications() async {
+    await AwesomeNotifications().initialize(
+        null,
+        // 'resource://drawable/res_app_icon',
+        [
+          NotificationChannel(
+            channelGroupKey: 'basic_channel_group',
+            channelKey: 'basic_channel',
+            channelName: 'Basic Notifications',
+            channelDescription: 'Basic Notifications channel',
+            defaultColor: AppColor.primaryColor,
+            importance: NotificationImportance.High,
+            ledColor: Colors.white,
+          )
+        ],
+        debug: true);
+
+    initialAction = await AwesomeNotifications()
+        .getInitialNotificationAction(removeFromActionEvents: false);
+  }
+
+  // NOTIFICATION EVENTS LISTENER
+
+  static Future<void> startListeningNotificationEvents() async {
+    AwesomeNotifications()
+        .setListeners(onActionReceivedMethod: onActionReceivedMethod);
+  }
+
   @pragma("vm:entry-point")
   static Future<void> onNotificationCreatedMethod(
       ReceivedNotification receivedNotification) async {}
@@ -18,4 +52,59 @@ class NotificationController {
   @pragma("vm:entry-point")
   static Future<void> onActionReceivedMethod(
       ReceivedAction receivedAction) async {}
+
+  // Create Timer Notification
+  static Future<void> createTimerDoneNotification(BuildContext context) async {
+    bool isAllowed = await AwesomeNotifications().isNotificationAllowed();
+    if (!isAllowed) {
+      // If permission is still not granted, return
+      return;
+    }
+    await AwesomeNotifications().createNotification(
+        content: NotificationContent(
+      id: DateTime.now().millisecondsSinceEpoch.remainder(1000),
+      title: 'FocusNest',
+      body:
+          'Congratulations! You have completed the task ${Emojis.hand_clapping_hands}',
+      channelKey: 'basic_channel',
+    ));
+  }
+}
+
+//  REQUESTING NOTIFICATION PERMISSIONS
+void displayRequestPermission(BuildContext context) async {
+  AwesomeNotifications().isNotificationAllowed().then(
+    (isAllowed) {
+      if (!isAllowed) {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Allow Notifications'),
+            content: const Text('Our app would like to send you notifications'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  context.pop();
+                },
+                child: const CustomText(
+                  title: 'Deny',
+                  color: AppColor.greyColor,
+                ),
+              ),
+              TextButton(
+                onPressed: () => AwesomeNotifications()
+                    .requestPermissionToSendNotifications()
+                    .then((_) => context.pop()),
+                child: const CustomText(
+                  title: 'Allow',
+                  color: AppColor.primaryColor,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    },
+  );
 }
