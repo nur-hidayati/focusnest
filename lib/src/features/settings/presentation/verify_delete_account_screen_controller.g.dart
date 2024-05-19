97b4aef7aced7bc3edaf1637c62e767eb26ed631
nur-hidayati <hidayati.coder@gmail.com>
2024-05-20 06:07:06 +0800

    add VerifyDeleteAccountScreenController

---
+++ lib/src/features/settings/presentation/verify_delete_account_screen_controller.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'verify_delete_account_screen_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$verifyDeleteAccountScreenControllerHash() =>
+    r'9106ae45e66addd18b0309a4109a490bd57fbd39';
+
+/// See also [VerifyDeleteAccountScreenController].
+@ProviderFor(VerifyDeleteAccountScreenController)
+final verifyDeleteAccountScreenControllerProvider =
+    AutoDisposeAsyncNotifierProvider<VerifyDeleteAccountScreenController,
+        void>.internal(
+  VerifyDeleteAccountScreenController.new,
+  name: r'verifyDeleteAccountScreenControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$verifyDeleteAccountScreenControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$VerifyDeleteAccountScreenController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
