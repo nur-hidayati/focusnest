86cee3ac299758ca6d592de7c95ffd47209bc7cd
nur-hidayati <hidayati.coder@gmail.com>
2024-05-20 21:39:38 +0800

    code review SettingsScreen

--- lib/src/features/settings/presentation/settings_screen.dart
+++ lib/src/features/settings/presentation/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:focusnest/src/common_widgets/link_text_button.dart';
+import 'package:focusnest/src/common_widgets/loading_manager.dart';
 import 'package:focusnest/src/constants/routes_name.dart';
 import 'package:focusnest/src/constants/spacers.dart';
 import 'package:focusnest/src/constants/strings.dart';
@@ -52,6 +53,39 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
     }
   }
 
+  void _handleNavigateToUserSettings({String? userId, String? userEmail}) {
+    if (userId != null && userEmail != null) {
+      context.pushNamed(
+        RoutesName.accountSettings,
+        pathParameters: {
+          'userId': userId,
+        },
+        queryParameters: {
+          'userEmail': userEmail,
+        },
+      );
+    } else {
+      showOKAlert(
+        context: context,
+        title: 'Error',
+        content: 'User not login',
+      );
+    }
+  }
+
+  void _handleSignOut() async {
+    final logoutConfirmed = await showAlertDialog(
+      context: context,
+      title: 'Confirm Sign Out',
+      content:
+          'Are you sure you want to sign out of FocusNest? This will end your current session.',
+      defaultActionText: 'Yes',
+    );
+    if (logoutConfirmed == true) {
+      ref.read(settingsScreenControllerProvider.notifier).signOut();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final authRepository = ref.watch(authRepositoryProvider);
@@ -62,99 +96,68 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
       settingsScreenControllerProvider,
       (_, state) => state.showAlertDialogOnError(context),
     );
-    final state = ref.watch(settingsScreenControllerProvider);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ListView(
-              shrinkWrap: true,
-              children: [
-                SettingTile(
-                  title: 'Account Settings',
-                  subtitle: 'Manage your account settings and preferences',
-                  icon: Icons.account_circle_outlined,
-                  action: () {
-                    if (userId != null) {
-                      context.pushNamed(
-                        RoutesName.accountSettings,
-                        pathParameters: {
-                          'userId': userId,
-                        },
-                        queryParameters: {
-                          'userEmail': userEmail,
-                        },
-                      );
-                    } else {
-                      showOKAlert(
-                        context: context,
-                        title: 'Error',
-                        content: 'User not login',
-                      );
-                    }
-                  },
-                ),
-                SettingTile(
-                  title: 'Privacy Policy',
-                  subtitle: 'Learn how we handle and protect your data',
-                  icon: Icons.privacy_tip_outlined,
-                  action: () {
-                    _launchURL('https://focusnest-app.github.io/focusnest/');
-                  },
-                ),
-                SettingTile(
-                  title: 'Terms of Service',
-                  subtitle: 'Understand your rights and obligations',
-                  icon: Icons.description_outlined,
-                  action: () {
-                    _launchURL(
-                        'https://focusnest-app.github.io/focusnest/terms_of_use');
-                  },
-                ),
-                const SettingTile(
-                  title: 'Contact Us',
-                  subtitle: 'Reach out to us at ${Strings.emailName}',
-                  icon: Icons.mail_outline,
-                  hasTrailingIcon: false,
-                ),
-                SettingTile(
-                  title: 'App Version',
-                  subtitle: 'Current app version: ${_packageInfo.version}',
-                  icon: Icons.info_outline,
-                  hasTrailingIcon: false,
-                ),
-              ],
-            ),
-            Spacers.mediumVertical,
-            LinkTextButton(
-              title: 'Sign Out',
-              onPressed: state.isLoading
-                  ? null
-                  : () async {
-                      final logoutConfirmed = await showAlertDialog(
-                        context: context,
-                        title: 'Confirm Sign Out',
-                        content:
-                            'Are you sure you want to sign out of FocusNest? This will end your current session.',
-                        defaultActionText: 'Yes',
-                      );
-                      if (logoutConfirmed == true) {
-                        ref
-                            .read(settingsScreenControllerProvider.notifier)
-                            .signOut();
-                      }
 
-                      if (logoutConfirmed == true) {
-                        ref
-                            .read(settingsScreenControllerProvider.notifier)
-                            .signOut();
-                      }
+    final state = ref.watch(settingsScreenControllerProvider);
+
+    return LoadingManager(
+      isLoading: state.isLoading,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Settings'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  SettingTile(
+                    title: 'Account Settings',
+                    subtitle: 'Manage your account settings and preferences',
+                    icon: Icons.account_circle_outlined,
+                    action: () => _handleNavigateToUserSettings(
+                      userId: userId,
+                      userEmail: userEmail,
+                    ),
+                  ),
+                  SettingTile(
+                    title: 'Privacy Policy',
+                    subtitle: 'Learn how we handle and protect your data',
+                    icon: Icons.privacy_tip_outlined,
+                    action: () {
+                      _launchURL('https://focusnest-app.github.io/focusnest/');
                     },
-            ),
-          ],
+                  ),
+                  SettingTile(
+                    title: 'Terms of Service',
+                    subtitle: 'Understand your rights and obligations',
+                    icon: Icons.description_outlined,
+                    action: () {
+                      _launchURL(
+                          'https://focusnest-app.github.io/focusnest/terms_of_use');
+                    },
+                  ),
+                  const SettingTile(
+                    title: 'Contact Us',
+                    subtitle: 'Reach out to us at ${Strings.emailName}',
+                    icon: Icons.mail_outline,
+                    hasTrailingIcon: false,
+                  ),
+                  SettingTile(
+                    title: 'App Version',
+                    subtitle: 'Current app version: ${_packageInfo.version}',
+                    icon: Icons.info_outline,
+                    hasTrailingIcon: false,
+                  ),
+                ],
+              ),
+              Spacers.mediumVertical,
+              LinkTextButton(
+                title: 'Sign Out',
+                onPressed: _handleSignOut,
+              ),
+            ],
+          ),
         ),
       ),
     );
