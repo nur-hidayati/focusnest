9e7135754a559e372fa2f2374de52c5ca5eb0d80
nur-hidayati <hidayati.coder@gmail.com>
2024-05-21 18:52:13 +0800

    add all images related

--- lib/main_common.dart
+++ lib/main_common.dart
@@ -1,13 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:focusnest/src/services/notification_controller.dart';
 
 import 'app.dart';
 
 Future<void> mainCommon(FirebaseOptions firebaseOptions, String flavor) async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
@@ -24,4 +26,8 @@ Future<void> mainCommon(FirebaseOptions firebaseOptions, String flavor) async {
       child: App(flavor: flavor),
     ),
   );
+}
+
+void removeSplashScreen() {
+  FlutterNativeSplash.remove();
 }
