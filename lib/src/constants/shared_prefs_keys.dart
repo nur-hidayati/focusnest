d01157638aa0d92d90cfe7c0eb9ad3c0494afdef
nur-hidayati <hidayati.coder@gmail.com>
2024-05-26 04:47:01 +0800

    add SharedPrefsKeys consts

---
+++ lib/src/constants/shared_prefs_keys.dart
@@ -0,0 +1,6 @@
+class SharedPrefsKeys {
+  static const String timerSeconds = 'timer_seconds';
+  static const String savedTime = 'saved_time';
+  static const String isForeground = 'is_foreground';
+  static const String lastTimerId = 'last_timer_id';
+}
